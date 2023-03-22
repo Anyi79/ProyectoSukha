dd9daf2aa5505d9d947c3149e28d88052ff14b5f
Delia MunMol <delia.m.m@outlook.es>
2023-03-22 09:51:32 +0100

    Finished divider

--- ProyectoSukha/src/components/Divider.jsx
+++ ProyectoSukha/src/components/Divider.jsx
@@ -1,12 +1,11 @@
 import "../Style/Divider.css";
 
-
 function LandingDivider () {
     return (
     <>
         <container className="Container"> 
             <div className="Content"> 
-                <img className="Logo" src="src\Img\logo left.png" alt="" /> <p>Despierta tu esencia, transforma tu vida</p> <img className="Logo" src="src\Img\Logo right.png" alt="" />
+            <img className="Logo" src="src\Img\logoleft.png" alt="" /><p>Despierta tu esencia, transforma tu vida</p>  <img className="Logo" src="src\Img\logoright.png" alt="" />
             </div>
         
         </container>
