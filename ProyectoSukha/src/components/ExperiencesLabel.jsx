7c71a12cd26bd1df6f713775a2d63e8902deb729
Delia MunMol <delia.m.m@outlook.es>
2023-03-22 14:06:32 +0100

    anchor fixed

--- ProyectoSukha/src/components/ExperiencesLabel.jsx
+++ ProyectoSukha/src/components/ExperiencesLabel.jsx
@@ -9,7 +9,7 @@ function Experiences () {
                     <div id="Container1">
                     <h2>Clases de aromaterapia</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel vestibulum enim. Mauris nec faucibus enim. Pellentesque velit lectus, maximus sit amet blandit non, maximus et nisi. Sed et purus ullamcorper, pulvinar dolor finibus, aliquam eros. Maecenas fermentum at ipsum id semper. Sed semper risus cursus, placerat mauris ac, molestie justo. Nulla interdum finibus libero.</p>
-                    <a>¡Pregunta aquí!</a> 
+                    <button>¡Pregunta aquí!</button> 
                     </div>
                     <div id="img1"></div>
                 </div>
@@ -17,7 +17,7 @@ function Experiences () {
                     <div id="Container2">
                     <h2>Sesiones de Reiki</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel vestibulum enim. Mauris nec faucibus enim. Pellentesque velit lectus, maximus sit amet blandit non, maximus et nisi. Sed et purus ullamcorper, pulvinar dolor finibus, aliquam eros. Maecenas fermentum at ipsum id semper. Sed semper risus cursus, placerat mauris ac, molestie justo. Nulla interdum finibus libero.</p>
-                    <a>¡Pregunta aquí!</a>
+                    <button>¡Pregunta aquí!</button> 
                     </div>
                     <div id="img2"></div>
                 </div>
