72028db9101ab8b3d50b110928cf9a9c2d01458b
Delia MunMol <delia.m.m@outlook.es>
2023-03-28 15:13:04 +0200

    Navar update

--- ProyectoSukha/src/components/Navbar.jsx
+++ ProyectoSukha/src/components/Navbar.jsx
@@ -23,9 +23,7 @@ function NavScroll() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
              <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll>
+              className="me-auto my-2 my-lg-0">
               
               <Col className="d-flex flex-grow-1">
          
@@ -68,34 +66,7 @@ function NavScroll() {
                 </Col>
               </div>
 
-
-              {/*             <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link> */}
-      
-
-
-            {/*  <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form> */}
-
-</Nav>
+          </Nav>
           </Navbar.Collapse>
         </Row>
       </Container>
