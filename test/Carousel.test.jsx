6d000ed0c007951cbbd5658ca096b92f4d5b85f0
Rosemary <Rosemary>
2023-03-28 21:38:44 +0200

    add file Carousel.test.jsx

--- test/Carousel.test.jsx
+++ test/Carousel.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Carousel from './Carousel';
+import Carousel from './test/Carousel.test.jsx';
 import '@testing-library/jest-dom';
 test('should contain a link element', () => {
     render(<Carousel />);
