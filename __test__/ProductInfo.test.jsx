d64ec1ec7cb0602be65fa587b73d720c40068786
Rosemary <Rosemary>
2023-03-30 13:57:49 +0200

    add change in ProductInfo.test.jsx

--- __test__/ProductInfo.test.jsx
+++ __test__/ProductInfo.test.jsx
@@ -1,31 +1,35 @@
-import { render, screen} from "@testing-library/react";
-import ProductInfo from "../src/components/ProductInfo.jsx";
+import { render, screen } from "@testing-library/react";
+import ProductInfo from "../src/components/ProductInfo";
 import '@testing-library/jest-dom';
-
-test("should render product title ", () => {
-    render (<ProductInfo />);
-    const productTitle = screen.getByText(/Aceites/);
-    expect(productTitle).toBeInTheDocument();
-
-})
 
 vi.mock('react-router-dom', () => ({
     useLoaderData: vi.fn(() => ({
-    product: {
-    id: 1,
-    name: "Aceites",
-    description: "ingredientes naturales",
-    category: "Aceites",
-    brand: "Oil",
-    price: 9.9,
-    //image: 'https://example.com/product.jpg',
-    },
+        product: {
+            id: 1,
+            name: "Aceites",
+            description: "ingredientes naturales",
+            category: "Aceites",
+            brand: "Oil",
+            price: 9.9,
+            image: 'https://example.com/aceites.jpg',
+        },
     })),
-    }));
+}));
 
-    describe("product", () => {
-        beforeEach(() => {
-            render(<ProductInfo />);
-        })
+describe("product", () => {
+    beforeEach(() => {
+        render(<ProductInfo />);
     })
+
+    test("should render product title ", () => {
+        screen.debug();
+        const productTitle = screen.getAllByText("Aceites");
+        expect(productTitle[0]).toBeInTheDocument();
+    
+    })
+})
+
+
+
+
 
